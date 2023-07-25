cd5763171a5ae36ed5ac45ebd876326805c48ed4
Kasif Ali <kasifaliplk07860@gmail.com>
2023-07-25 16:51:28 +0530

    completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
-  
+
+    static class A{
+        A(){};
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    static class B extends A{
+        B(){};
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        B b = new B();
+        b.meth();
+    }
+
+
 }
+
